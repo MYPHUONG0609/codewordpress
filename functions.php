96256d8c788f853eacc50b8d76f3532fe19b5fa6
MYPHUONG0609 <53520113+MYPHUONG0609@users.noreply.github.com>
2021-01-23 21:06:33 +0700

    Add files via upload

--- functions.php
+++ functions.php
@@ -24,7 +24,33 @@ if ( !function_exists('thachpham_theme_setup')){
 		$language_folder= THEME_URL .'/languages';
 		load_theme_textdomain('thachpham',$language_folder);
 /*tu dong them link RSS len <head> */
-		
+		add_theme_support('automatic-feed-links');
+		add_theme_support('post-thumbnails');
+		add_theme_support('post-formats', array(
+			'image',
+			'video',
+			'gallery',
+			'quote',
+			'link',
+		)
+		);
+
+		add_theme_support('title-tag');
+		$default_background =  array('default-color' => '#e8e8e8' );
+		add_theme_support('custom-background');
+		/** them menu**/
+		register_nav_menu('primary-menu',__('Primary Menu','thachpham'));
+		$sidebar = array(
+			'name' =>__('Main Sidebar' ,'thachpham' ),
+			'id'=> 'main-sidebar',
+			'description'=>__ ('default sidebar'),
+			'class' => 'main-sidebar',
+			'before_title'=> '<h3 class="widgettitle">',
+			'after_title'=> '</h3>'
+			); 
+			register_sidebar($sidebar);
+			
+			 
 	}	
 	add_action('init','thachpham_theme_setup');
 
